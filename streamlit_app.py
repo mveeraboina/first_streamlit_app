951524c933a77f7c9ecedfb90122b537d9bbb891
mveeraboina <118164423+mveeraboina@users.noreply.github.com>
2022-11-14 04:36:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit
 streamlit.title("My Mom's New Healthy Dinner")
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(':avocado: Omega 3 & Bluebeery Oatmeal')
+streamlit.text( 'Omega 3 & Bluebeery Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
+
+
+
